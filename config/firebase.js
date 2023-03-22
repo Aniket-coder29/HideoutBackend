2c6d58728190f878f9143a4aae9d922ff9d88183
Aniket-coder29 <aniket.balodia.ug20@nsut.ac.in>
2023-03-22 23:43:35 +0530

    changed env

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,17 @@
 const { credential } = require('firebase-admin');
 const { initializeApp } = require('firebase-admin/app');
-const serviceAccount = require("../octochat-48012-5ca6d4fab505.json");
+const serviceAccount = {
+    "type": process.env.FIREBASE_PROJECT_TYPE,
+    "project_id": process.env.FIREBASE_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_PROJECT_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_PROJECT_PRIVATE_KEY,
+    "client_email": process.env.FIREBASE_PROJECT_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_PROJECT_CLIENT_ID,
+    "auth_uri": process.env.FIREBASE_PROJECT_AUTH_URI,
+    "token_uri": process.env.FIREBASE_PROJECT_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_PROJECT_AUTH_PROVIDER_CERT_URL,
+    "client_x509_cert_url": process.env.FIREBASE_PROJECT_CLIENT_CERT_URL
+  }
 
 const app = initializeApp({
     credential: credential.cert(serviceAccount)
