9b23c4c27dc99fa4e5b0752f35dfa618f80a726b
Aniket-coder29 <aniket.balodia.ug20@nsut.ac.in>
2023-04-13 17:04:07 +0530

    Post routes working now

--- controller/PostController.js
+++ controller/PostController.js
@@ -1,61 +1,19 @@
 const Post = require('../models/post');
 const Friend = require('../models/friend');
 const { getFriends } = require('../services/friendServices');
-const { getPost, getAllPost } = require('../services/postServices');
+const { getPost, getAllPost, compilePosts2, delete_post } = require('../services/postServices');
 const { getMinDetails } = require('../services/userServices');
 
 const getAllPosts = async (req, res, next) => {
     if (res.locals.user) {
         try {
             const user = res.locals.user;
-            // const allfriends = await getFriends(user.uid)
-            // console.log(allfriends)
-            // let posts = []
-            // if(allfriends.status){
-            //     const allIds = new Set();
-            //     const friends = allfriends.data.friends;
-            //     console.log("friends array",friends)
-            //     friends.forEach(element => {
-            //         allIds.add(element)
-            //     });
-            //     const details = await getMinDetails(user.uid)
-            //     allIds.add(details.data)
-            //     // console.log("allIds",allIds)
-            //     // console.log(allIds)
-            //     allIds.forEach(async(ele)=>{
-            //         // console.log("ele",ele)
-            //         const post = await getPost(ele.uid)
-            //         // console.log("post",post)
-                    
-                    
-            //         // const details = await getMinDetails(id)
-            //         if(post.status){
-            //             // console.log("postdata",post.data)
-            //             if(post.data){
-            //                 post.data.posts.forEach((pos)=>{
-            //                     // console.log("pos",pos)
-            //                     const rt = {...pos.toJSON(),...ele.toJSON()}
-            //                     // console.log("rt",rt)
-            //                     posts.push(rt)
-            //                 })
-            //                 // const ret = {...post.data,...ele}
-            //                 // console.log("ret",ret)
-            //                 // posts.push(post.data)
-            //             }
-            //         }
-            //     })
-            //     // console.log("posts",posts)
-            // }
-            // console.log("posts",posts)
-            // const friends = allfriends.friends;
-            //add all ids to array
-            // const posts = await Post.find({ userid: user.uid });
             const posts = await getAllPost(user.uid)
-            console.log("get posts in post controller",posts)
-            if(posts.status){
+            // console.log("get posts in post controller",posts)
+            if (posts.status) {
                 res.status(200).json(posts.data);
             }
-            else{
+            else {
                 res.status(500).json(posts.error)
             }
 
@@ -81,21 +39,12 @@ const getUserPost = async (req, res, next) => {
             if (!uid) {
                 return res.status(500).json({ "error": "no uid passed" })
             }
-            console.log(uid)
-            const filter = { uid: uid }
-            const posts = await Post.findOne(filter).clone().exec();
-            console.log(posts)
-            // const posts = await Post.find(filter, async (err, docs) => {
-            //     if (err) {
-            //         // console.log(err)
-            //         res.status(200).json(err)
-            //     }
-            //     else {
-            //         // console.log(docs)
-            //         res.status(200).json(docs)
-            //     }
-            // });
-            res.status(200).json(posts);
+            // console.log(uid)
+            const details = await getMinDetails(uid)
+            // console.log(details)
+            const post = await compilePosts2(details.data)
+            // console.log(post)
+            res.status(200).json(post);
 
         } catch (error) {
             console.log(error)
@@ -118,45 +67,22 @@ const makePost = async (req, res) => {
         const filter = { uid: user.uid }
         try {
             const PostMaker = await Post.findOne(filter).clone().exec();
-            console.log(PostMaker)
-            if(!PostMaker){
+            // console.log(PostMaker)
+            if (!PostMaker) {
                 const newPost = new Post(filter);
                 const save = await newPost.save();
-                console.log(save)
+                // console.log(save)
             }
-            const addPost = await Post.findOneAndUpdate(filter,{ $push: {posts:req.body} }).clone().exec();
-            console.log(addPost)
+            const addPost = await Post.findOneAndUpdate(filter, { $push: { posts: req.body } }).clone().exec();
+            // console.log(addPost)
             res.status(200).json({
-                status:1
+                status: 1
             })
-            // await Post.findOne(filter, async (err, docs) => {
-            //     if (err) {
-            //         // return false;
-            //     }
-            //     else {
-            //         const newPost = new Post(filter);
-            //         const save = newPost.save()
-            //         console.log(req.body)
-            //         Post.findOneAndUpdate(filter, { $push: {posts:req.body} }, async (err, docs) => {
-            //             if (err) {
-            //                 res.status(400).json({
-            //                     'status': 'Not able to Post! Try Again'
-            //                 })
-            //             }
-            //             else {
-            //                 res.status(200).json({
-            //                     'success': "Successfully posted"
-            //                 })
-            //             }
-            //         }).clone()
-            //     }
-            // }).clone()
-
         }
         catch (error) {
             res.status(500).json({
-                status:0,
-                error:error
+                status: 0,
+                error: error
             });
         }
     }
@@ -175,16 +101,17 @@ const deletePost = async (req, res) => {
             const user = res.locals.user
             const filter = { uid: user.uid }
             const postId = req.query.postId
-            const delPost = Post.findOneAndUpdate(filter, { $pullAll: { posts: postId } }, async (err, docs) => {
-                if (err) {
-                    console.log(err)
-                    res.status(200).json(err)
-                }
-                else {
-                    // console.log(docs)
-                    res.status(200).json(docs)
-                }
-            })
+            console.log(user.uid,postId)
+            const delPost = await delete_post(user.uid, postId)
+            console.log(delPost)
+            if (delPost.status) {
+                res.status(200).json({ status: "Successfully deleted" })
+            }
+            else{
+                res.status(500).json({
+                    error: delPost.error
+                })
+            }
         } catch (error) {
             res.status(500).json(error)
         }
--- services/postServices.js
+++ services/postServices.js
@@ -1,12 +1,12 @@
 const Post = require('../models/post');
-const {getFriends} = require('../services/friendServices')
-const {getMinDetails} = require('../services/userServices')
+const { getFriends } = require('../services/friendServices')
+const { getMinDetails } = require('../services/userServices')
+
 const getPost = async (id) => {
-    console.log("get post from post services")
-    const filter = {uid:id}
+    // console.log("get post from post services")
+    const filter = { uid: id }
     try {
         const posts = await Post.findOne(filter).clone().exec();
-        console.log(posts)
         return {
             status: 1,
             data: posts
@@ -19,48 +19,41 @@ const getPost = async (id) => {
     }
 }
 
-const compilePosts2 = async(ele)=>{
+const compilePosts2 = async (ele) => {
     let posts = []
     const post = await getPost(ele.uid)
-    if(post.status && post.data){
-        post.data.posts.forEach((pos)=>{
-            const rt = {...pos.toJSON(),...ele.toJSON()}
+    if (post.status && post.data) {
+        post.data.posts.forEach((pos) => {
+            const rt = { ...ele.toJSON(),...pos.toJSON() }
             posts.push(rt)
         })
     }
     return posts;
 }
 
-const compilePosts1 = async(allIds)=>{
+const compilePosts1 = async (allIds) => {
     let posts = []
     try {
-        for(let i of allIds){
-            // console.log(allIds[i])
-            // console.log(i)
+        for (let i of allIds) {
             const post = await compilePosts2(i)
-            // console.log(post)
-            post.forEach((pos)=>{
+            post.forEach((pos) => {
                 posts.push(pos)
             })
         }
-        // console.log("try part")
-        // console.log(posts)
     } catch (error) {
-        // console.log(error)
         return []
     }
-    finally{
-        // console.log("here")
+    finally {
         return posts
     }
 }
 
-const getAllPost = async(id)=>{
+const getAllPost = async (id) => {
     try {
         const allfriends = await getFriends(id)
         // console.log(allfriends)
         // let posts = []
-        if(allfriends.status){
+        if (allfriends.status) {
             const allIds = new Set();
             const friends = allfriends.data.friends;
             // console.log("friends array",friends)
@@ -72,23 +65,41 @@ const getAllPost = async(id)=>{
             const posts = await compilePosts1(allIds)
             // console.log(posts)
             return {
-                status:1,
-                data:posts
+                status: 1,
+                data: posts
             }
         }
-        else{
+        else {
             return {
-                status:0,
-                error:allfriends.error
+                status: 0,
+                error: allfriends.error
             }
         }
     } catch (error) {
         return {
-            status:0,
-            error:error
+            status: 0,
+            error: error
         }
     }
-    
+
 }
 
-module.exports = { getPost, getAllPost }
+const delete_post = async (id, postid) => {
+    const filter = { uid: id }
+    try {
+        const delPost = await Post.findOneAndUpdate(filter, { $pull: { posts: { _id: postid } } }).clone().exec();
+        console.log(delPost)
+        return {
+            status: 1
+        }
+    }
+    catch (error) {
+        return {
+            status: 0,
+            error: error
+        }
+    }
+
+}
+
+module.exports = { getPost, getAllPost, compilePosts2, delete_post }
